58fff013789028188a1df167ae7bee3fac5dfe78
Felix <111679444+mooseRobot@users.noreply.github.com>
2022-11-11 23:37:16 -1000

    Update Cag.py

--- Cag.py
+++ Cag.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-import praw, asyncio
+import praw, discord
 
 reddit = praw.Reddit(
 client_id="",
@@ -11,6 +11,7 @@ username="",
 
 reddit.read_only = True
 
+###### Global Grab Functions ######
 async def cag_grab(subr = "", lim=0):
     global gallery_url
     global title
@@ -37,6 +38,33 @@ async def cag_grab(subr = "", lim=0):
     except:
         return gallery_url
 
+async def post_type(subreddit) -> str:
+    global upvotes
+    global post_url
+    global redditor
+    global body
+    global title
+    upvotes = 0
+    post_url = ""
+    redditor = None
+    body = ""
+    title = ""
+
+    global text_post
+    text_post = False
+
+    hot_posts = reddit.subreddit(subreddit).hot(limit = 1)
+    for post in hot_posts:
+        if post.is_self:
+            redditor = post.author # redditor.name
+            upvotes = post.score
+            post_url = post.url
+            body = post.selftext 
+            title = post.title
+            text_post = True
+        else:
+            await cag_grab(subr = subreddit, lim=1)
+
 
 class Cag(commands.Cog):
 
@@ -46,6 +74,7 @@ class Cag(commands.Cog):
     # Grabs top hot post from r/kpics
     @commands.command()
     async def cag(self, ctx):
+        await ctx.channel.send("Grabbing image <:masaka:706682412943540244>", delete_after=2)
         # Calls global function cag_grab with parameter
         await cag_grab(subr = "kpics", lim=1)
         await ctx.channel.send(title)
@@ -67,7 +96,28 @@ class Cag(commands.Cog):
             for url in gallery:
                 await ctx.channel.send(url)
         else:
-            await ctx.channel.send(gallery_url)    
+            await ctx.channel.send(gallery_url)
+
+    # Grabs top hot post from subreddit
+    @commands.command()
+    async def trending(self, ctx):
+        async with ctx.typing():
+            subreddit = str(ctx.message.content[10:])
+            await post_type(subreddit)
+            text = text_post
+            if text == True:
+                MyEmbed = discord.Embed(title=f"{title}", url=f"{post_url}", color=0xbd0000)
+                MyEmbed.set_author(name=f"r/{subreddit}")
+                MyEmbed.add_field(name = f"{upvotes} upvotes", value = f"{body}")
+                await ctx.send(embed=MyEmbed)
+                text = False
+            else:
+                await ctx.channel.send(title)
+                if len(gallery) > 0:
+                    for url in gallery:
+                        await ctx.channel.send(url)
+                else:
+                    await ctx.channel.send(gallery_url)
 
 def setup(bot):
     bot.add_cog(Cag(bot))
